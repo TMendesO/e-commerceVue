7c1bb1ea948e5461436023bb5ff4b6206b5d5c81
Thiago Mendes <thiago.mendes777@hotmail.com>
2022-03-02 19:12:20 -0300

    finalizado o modal

--- app.js
+++ app.js
@@ -2,6 +2,15 @@ const vm = new Vue({
   el: "#app",
   data: {
     produtos: [],
+    produto: false,
+  },
+  filters: {
+    numeroPreco(value) {
+      return value.toLocaleString("pt-BR", {
+        style: "currency",
+        currency: "BRL",
+      });
+    },
   },
   methods: {
     fetchProdutos() {
@@ -10,6 +19,22 @@ const vm = new Vue({
         .then((r) => {
           this.produtos = r;
         });
+    },
+    fetchProduto(id) {
+      fetch(`./api/produtos/${id}/dados.json`)
+        .then((r) => r.json())
+        .then((r) => {
+          this.produto = r;
+        });
+    },
+    abrirModal(id) {
+      this.fetchProduto(id);
+      window.scrollTo({
+        top: 0,
+      });
+    },
+    fecharModal(event) {
+      if (event.target === event.currentTarget) this.produto = false;
     },
   },
   created() {
