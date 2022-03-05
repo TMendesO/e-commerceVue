f709378af786f4708c9669daf8eec74d2e004576
Thiago Mendes <thiago.mendes777@hotmail.com>
2022-03-04 21:47:09 -0300

    0.4.0 estilizado o carrinho

--- app.js
+++ app.js
@@ -3,6 +3,10 @@ const vm = new Vue({
   data: {
     produtos: [],
     produto: false,
+    carrinho: [],
+    carrinhoAtivo: false,
+    mensagemAlerta: "Item acicionado",
+    alertaAtivo: false,
   },
   filters: {
     numeroPreco(value) {
@@ -10,6 +14,17 @@ const vm = new Vue({
         style: "currency",
         currency: "BRL",
       });
+    },
+  },
+  computed: {
+    carrinhoTotal() {
+      let total = 0;
+      if (this.carrinho.length) {
+        this.carrinho.forEach((item) => {
+          total += item.preco;
+        });
+      }
+      return total;
     },
   },
   methods: {
@@ -36,8 +51,47 @@ const vm = new Vue({
     fecharModal(event) {
       if (event.target === event.currentTarget) this.produto = false;
     },
+    clickForaCarrinho(event) {
+      if (event.target === event.currentTarget) this.carrinhoAtivo = false;
+    },
+    adicionarItem() {
+      this.produto.estoque--;
+      const { id, nome, preco } = this.produto;
+      this.carrinho.push({ id, nome, preco });
+      this.alerta(`${nome} adicionado ao carrinho.`);
+    },
+    removerItem(index) {
+      this.carrinho.splice(index, 1);
+    },
+    checarLocalStorage() {
+      if (window.localStorage.carrinho)
+        this.carrinho = JSON.parse(window.localStorage.carrinho);
+    },
+    alerta(mensagem) {
+      this.mensagemAlerta = mensagem;
+      this.alertaAtivo = true;
+      setTimeout(() => {
+        this.alertaAtivo = false;
+      }, 1000);
+    },
+    router() {
+      const hash = document.location.hash;
+      if (hash) this.fetchProduto(hash.replace("#", ""));
+    },
+  },
+  watch: {
+    produto() {
+      document.title = this.produto.nome || "Techno";
+      const hash = this.produto.id || "";
+      history.pushState(null, null, `#${hash}`);
+    },
+    carrinho() {
+      window.localStorage.carrinho = JSON.stringify(this.carrinho);
+    },
   },
   created() {
     this.fetchProdutos();
+    this.checarLocalStorage();
+    this.router();
   },
 });
